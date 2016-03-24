49f1630f2d5adb15ebadf384b3ab080d352d42fa
Anthony Gold and Tommy Jones <anthony@>
2016-03-24 16:14:39 -0700

    working but buggy- wont get past nAt

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
   // Set value of sides
-  $("#sides").submit(function(event) {
-    var side1 = parseInt($("input#side1")val());
-    var side2 = parseInt($("input#side2")val());
-    var side3 = parseInt($("input#side3")val());
+  $("form").submit(function(event) {
+    var side1= $("#side1").val();
+    var side2= $("#side2").val();
+    var side3= $("#side3").val();
+    console.log(side1, side2, side3);
 
-    if (side1, side3 === side2) {
-    $("#equilateral").show();
-  } else if (){
-    (COMPARISON)
-    $('#isosceles').show();
-  } else if (){
-    (COMPARISON)
-    $('#scalene').show();
-  } else (){
-    (COMPARISON)
-    $('#not-a-triangle').show();
-  }
+    if (side1 === side2 && side2 === side3 && side1 != 0){
+    $('#equilateral').show();
+    } else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
+      $('#not-a-triangle').show();
+    } else if (side1 === side2 && side1 != side3 || side2 === side3 && side2 != side1 || side1=== side3 && side1 != side2) {
+      $('#isosceles').show();
+    } else if (side1 != side2 && side1 != side3 || side2!= side3 && side2 != side1 || side3 != side1 && side3 != side2){
+      $('#scalene').show();
+    }
     //check syntax of else
     event.preventDefault();
   });
