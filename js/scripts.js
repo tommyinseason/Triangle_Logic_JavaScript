e372512c6d99662bfb7949c127cd3162f2001db1
Anthony Gold and Tommy Jones <tommy@>
2016-03-24 16:44:07 -0700

    Triangle Project with equilateral, isosceles and scalene

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
     console.log(side1, side2, side3);
 
     if (side1 === side2 && side2 === side3 && side1 != 0){
-    $('#equilateral').show();
-    } else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
-      $('#not-a-triangle').show();
-    } else if (side1 === side2 && side1 != side3 || side2 === side3 && side2 != side1 || side1=== side3 && side1 != side2) {
-      $('#isosceles').show();
-    } else if (side1 != side2 && side1 != side3 || side2!= side3 && side2 != side1 || side3 != side1 && side3 != side2){
-      $('#scalene').show();
-    }
+    $("#equilateral").show();
+    } else if (side1 === side2 && side1 != side3 || side2 === side3 && side2 != side1 || side1=== side3 && side1 != side2){
+    $('#isosceles').show();
+    } else if (side1 != side2 && side1 != side3 || side2 != side3 && side2 != side1 || side3 != side1 && side3){
+    $('#scalene').show();
+    } else {
+    $('#not-a-triangle').show();
+  }
     //check syntax of else
     event.preventDefault();
   });
